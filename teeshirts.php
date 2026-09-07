56445d52eaef8ce36f7e23398f1ef8bc1a4b756b
Angela-007 <e2538350@cmaisonneuve.qc.ca>
2026-09-07 17:29:33 -0400

    Rien fait

--- teeshirts.php
+++ teeshirts.php
@@ -1,19 +1,18 @@
 <?php
-    // Inclure le fichier commun contenant le haut du code des ecrans de UI
-    include_once('commun/entete.inc.php');
+// Inclure le fichier commun contenant le haut du code des ecrans de UI
+include_once('commun/entete.inc.php');
 ?>
 
-        <main class="page-teeshirts">
-            <article class="amorce">
-                <h1>Nos teeshirts</h1>
-            </article>
-            <article class="principal">
-                À venir...
-            </article>
-        </main>
+<main class="page-teeshirts">
+    <article class="amorce">
+        <h1>Nos teeshirts</h1>
+    </article>
+    <article class="principal">
+        À venir...
+    </article>
+</main>
 
 <?php
-    // Inclure le fichier commun contenant la partie du bas du code des ecrans de UI
-    include_once('commun/p2p.inc.php');
+// Inclure le fichier commun contenant la partie du bas du code des ecrans de UI
+include_once('commun/p2p.inc.php');
 ?>
-        
